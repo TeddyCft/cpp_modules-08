dbb3fcd16ae699289f5278d06517f1e559de96cd
tcoeffet <tcoeffet@student.42.fr>
2026-04-20 15:12:37 +0200

    fixed ex00 which needed to use a stl algorithm even if not specified in the subject :))))

--- ex00/src/templates/easyfind.hpp
+++ ex00/src/templates/easyfind.hpp
@@ -1,16 +1,14 @@
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
+#include <algorithm>
+
 template <typename T>
 typename T::const_iterator	easyfind(const T &container, int tofind)
 {
-	typename T::const_iterator	it;
-	for (it = container.begin(); it != container.end(); it++)
-	{
-		if (*it == tofind)
-			return (it);
-	}
-	return (container.end());
+	typename T::const_iterator it;
+	it = std::find(container.begin(), container.end(), tofind);
+	return (it);
 }
 
 #endif
